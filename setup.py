558211f18905cfc41b0c2a9a031c5c31e56b70b2
Ehsan Ahmad Khan <ahsanmalik525.am@gmail.com>
2025-01-23 00:53:42 +0500

    test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description_content_type='text/markdown',
     author='feder-cr',
     author_email='federico.elia.majo@gmail.com',
-    url='https://github.com/feder-cr/lib_resume_builder_AIHawk',
+    url='https://github.com/ehsanahmadkhan525/lib_resume_builder_AIHawk',
     packages=find_packages(),
     install_requires=[ 
         'langchain',
